73ce28bfed21c77a187a184e6ec15c23e90e1100
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-08 17:55:11 +0500

    `Animated Product Items for CustomerLogin Screen`

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -2,13 +2,17 @@ import {StyleSheet, Text, View} from 'react-native';
 import {GestureHandlerRootView, State} from 'react-native-gesture-handler';
 
 import React from 'react';
+import CustomSafeAreaView from '@components/global/CustomSafeArea';
+import ProductSlider from '@components/login/ProductSlider';
 
 const CustomerLogin = () => {
   return (
     <GestureHandlerRootView style={styles.container}>
-        <View style={styles.container}>
-
-        </View>
+      <View style={styles.container}>
+        <CustomSafeAreaView>
+          <ProductSlider />
+        </CustomSafeAreaView>
+      </View>
     </GestureHandlerRootView>
   );
 };
