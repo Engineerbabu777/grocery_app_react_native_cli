93e6b6e44ef67865908c30aa43c5dddedf3ade2e
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-08 16:16:02 +0500

    `- Default Props and app Icons && maps configure`

--- ios/GroceryAppReactNativeCLI/AppDelegate.mm
+++ ios/GroceryAppReactNativeCLI/AppDelegate.mm
@@ -2,11 +2,14 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"GroceryAppReactNativeCLI";
+  [GMSServices provideAPIKey:@"YOUR API KEY"];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
