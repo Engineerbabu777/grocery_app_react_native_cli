30eeba5c5c8420a52e224394f913571dcc164392
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-09 14:26:43 +0500

    `Delivery Dashboard Done`

---
+++ src/features/delivery/DeliveryDashboard.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const DeliveryDashboard = (props: Props) => {
+  return (
+    <View>
+      <Text>DeliveryDashboard</Text>
+    </View>
+  );
+};
+
+export default DeliveryDashboard;
